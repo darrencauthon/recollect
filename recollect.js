3c28af468f5e4feeb34bdd2951ead9cf693b2789
Caleb Cauthon <calebcauthon@gmail.com>
2018-07-21 13:26:48 -0500

    Start searching

--- recollect.js
+++ recollect.js
@@ -1,1 +1,10 @@
-recollect.js
+$(document).ready(() => {
+  $('#recollect-text-input').keyup(() => {
+    const searchTerm = $('#recollect-text-input').val();
+    const matchingNodes = _.filter(records, node => {
+      return node.id.search(searchTerm) > -1
+    });
+
+    console.log('matchingNodes', matchingNodes);
+  });
+});
