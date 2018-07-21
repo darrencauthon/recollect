022b40944ce1cc0fc5c1f6686b90328be948e9b5
Caleb Cauthon <calebcauthon@gmail.com>
2018-07-21 14:41:42 -0500

    Make a separate function

--- recollect.js
+++ recollect.js
@@ -2,24 +2,28 @@ var newColor = 'orange';
 $(document).ready(() => {
   $('#recollect-text-input').keyup(() => {
     const searchTerm = $('#recollect-text-input').val().toLowerCase();
-    var nodeIdsToUpdate = _.filter(_.keys(nodesOnThePage._data), (id) => {
-      if (searchTerm.length > 0 && id.toLowerCase().search(searchTerm) > -1) {
-        return id;
-      }
-    });
-    nodesOnThePage.update(_.keys(nodesOnThePage._data).map(id => {
-      return {
-        id: id,
-        color: null,
-        font: null,
-      };
-    }));
-    nodesOnThePage.update(nodeIdsToUpdate.map(id => {
-      return {
-        id: id,
-        color: { background: newColor },
-        font: { size: 20 }
-      };
-    }));
+    highlightNodes(searchTerm);
   });
 });
+
+function highlightNodes(searchTerm) {
+  var nodeIdsToUpdate = _.filter(_.keys(nodesOnThePage._data), (id) => {
+    if (searchTerm.length > 0 && id.toLowerCase().search(searchTerm) > -1) {
+      return id;
+    }
+  });
+  nodesOnThePage.update(_.keys(nodesOnThePage._data).map(id => {
+    return {
+      id: id,
+      color: null,
+      font: null,
+    };
+  }));
+  nodesOnThePage.update(nodeIdsToUpdate.map(id => {
+    return {
+      id: id,
+      color: { background: newColor },
+      font: { size: 20 }
+    };
+  }));
+}
