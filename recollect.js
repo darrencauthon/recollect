93154edf5b143b8bbf961f7cb6e237bc152b921e
Caleb Cauthon <calebcauthon@gmail.com>
2018-07-21 17:36:32 -0500

    Use one input

--- recollect.js
+++ recollect.js
@@ -1,78 +1,86 @@
 var newColor = 'orange';
 $(document).ready(() => {
-  $('#recollect-text-input').keyup(() => {
-    const searchTerm = $('#recollect-text-input').val().toLowerCase();
+  $('#recollect-text-input, #newThing').keyup((el) => {
+    const searchTerm = $(el.target).val().toLowerCase();
     highlightNodes(searchTerm);
   });
 
   $('#update-nodes').click(() => {
     const searchTerm = $('#recollect-text-input').val().toLowerCase();
-
-    _.each(nodesOnThePage._data, node => {
-      nodesOnThePage.remove(node);
-    });
-
-    const exactRecordMatches = _.filter(getAllData().records, record => {
-      return record.id.toLowerCase().search(searchTerm) > -1;
-    });
-    const recordsLinkedDirectly = findRecordsLinkedToThese(exactRecordMatches);
-    const recordsLinkedBySkippingOne = findRecordsLinkedToThese(recordsLinkedDirectly);
-
-    _.each(exactRecordMatches, record => {
-      record.options = { color: { background: 'orange' } };
-      addRecordToNodesOnThePage(record);
-    });
-    _.each(recordsLinkedDirectly, record => {
-      record.options = { color: { background: '#42d242' } };
-      addRecordToNodesOnThePage(record);
-    });
-
-    _.each(recordsLinkedBySkippingOne, record => {
-      record.options = { color: { background: 'tan' } };
-      addRecordToNodesOnThePage(record);
-    });
-
-
-    function findRecordsLinkedToThese(records) {
-      return _.chain(records)
-        .map(record => findRecordsLinkedTo(record))
-        .flatten()
-        .value();
-    }
-
-    function findRecordsLinkedTo(record) {
-      var relationships = getAllRelationships();
-      return _.chain(relationships)
-        .filter(relationship => {
-          return (relationship.from === record.id) || (relationship.to === record.id);
-        })
-        .map(relationship => {
-          if (relationship.from === record.id) {
-            return relationship.to;
-          } else {
-            return relationship.from
-          }
-        })
-        .map(recordId => {
-          return _.find(getAllData().records, record => recordId === record.id)
-        })
-        .value();
-    };
+    bringForward(searchTerm);
   });
 
   $('#newThing').keypress(event => {
+    const typedTerm = $('#newThing').val();
     const ENTER = 13;
 
     if (event.which === ENTER) {
-      addNode($('#newThing').val());
-      $('#newThing').val('');
+      if (findMatchingNodeIds(typedTerm).length === 0) {
+        addNode(typedTerm);
+        $('#newThing').val('');
+      } else {
+        bringForward(typedTerm);
+      }
     }
   });
 });
 
+function bringForward(searchTerm) {
+  _.each(nodesOnThePage._data, node => {
+    nodesOnThePage.remove(node);
+  });
+
+  const exactRecordMatches = _.filter(getAllData().records, record => {
+    return record.id.toLowerCase().search(searchTerm.toLowerCase()) > -1;
+  });
+  const recordsLinkedDirectly = findRecordsLinkedToThese(exactRecordMatches);
+  const recordsLinkedBySkippingOne = findRecordsLinkedToThese(recordsLinkedDirectly);
+
+  _.each(exactRecordMatches, record => {
+    record.options = { color: { background: 'orange' } };
+    addRecordToNodesOnThePage(record);
+  });
+  _.each(recordsLinkedDirectly, record => {
+    record.options = { color: { background: '#42d242' } };
+    addRecordToNodesOnThePage(record);
+  });
+
+  _.each(recordsLinkedBySkippingOne, record => {
+    record.options = { color: { background: 'tan' } };
+    addRecordToNodesOnThePage(record);
+  });
+
+
+  function findRecordsLinkedToThese(records) {
+    return _.chain(records)
+      .map(record => findRecordsLinkedTo(record))
+      .flatten()
+      .value();
+  }
+
+  function findRecordsLinkedTo(record) {
+    var relationships = getAllRelationships();
+    return _.chain(relationships)
+      .filter(relationship => {
+        return (relationship.from === record.id) || (relationship.to === record.id);
+      })
+      .map(relationship => {
+        if (relationship.from === record.id) {
+          return relationship.to;
+        } else {
+          return relationship.from
+        }
+      })
+      .map(recordId => {
+        return _.find(getAllData().records, record => recordId === record.id)
+      })
+      .value();
+  };
+}
+
 function findMatchingNodeIds(searchTerm) {
   var nodeIdsToUpdate = _.filter(_.keys(nodesOnThePage._data), (id) => {
-    if (searchTerm.length > 0 && id.toLowerCase().search(searchTerm) > -1) {
+    if (searchTerm.length > 0 && id.toLowerCase().search(searchTerm.toLowerCase()) > -1) {
       return id;
     }
   });
